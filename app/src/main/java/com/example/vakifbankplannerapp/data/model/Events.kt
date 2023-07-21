5f931a3f2cccd54e727b69973d2ea25b11ab8259
Baran Dogan <baran.dgn98@gmail.com>
2023-07-21 14:34:30 +0300

    Planner App Second Commit - 2107

---
+++ app/src/main/java/com/example/vakifbankplannerapp/data/model/Events.kt
@@ -0,0 +1,9 @@
+package com.example.vakifbankplannerapp.data.model
+
+data class Events(
+    val eventName : String,
+    val eventTeam : String,
+    val eventDate : String,
+    val eventContent : String,
+    val eventNotes : String,
+)
---
+++ app/src/main/java/com/example/vakifbankplannerapp/data/model/Teams.kt
@@ -0,0 +1,9 @@
+package com.example.vakifbankplannerapp.data.model
+
+data class Teams(
+    val teamName : String,
+    val meetingType : String,
+    val meetingDate : String,
+    val meetingContent : String,
+    val meetingNotes : String,
+)
