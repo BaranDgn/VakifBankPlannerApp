e0f7fd3e9675a48764d9d1ce9a2867b1f9e85073
Baran Dogan <baran.dgn98@gmail.com>
2023-07-26 15:09:35 +0300

    test

--- app/src/main/java/com/example/vakifbankplannerapp/domain/util/Constant.kt
+++ app/src/main/java/com/example/vakifbankplannerapp/domain/util/Constant.kt
@@ -3,4 +3,5 @@ package com.example.vakifbankplannerapp.domain.util
 object Constant {
 
     const val BASE_URL_FOR_LOGIN_AZURE = "https://vkfbnkmeetingapp.azurewebsites.net/"
+    const val TEST = "TEST"
 }
